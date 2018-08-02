3fc92e52f395bc004972e465c5004ba7fe9df613
Eric Hall <ericmbf@gmail.com>
2018-08-02 15:20:58 -0300

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :posts do
+  resources :comments
+  end
+  resources :categories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
+end
